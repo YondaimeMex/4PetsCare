a86b7f64ea305b49a3969a82f7ff89c7e65a25b3
YondaimeWindows <interesxtv@gmail.com>
2025-11-22 01:51:30 -0500

    Byscript

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-
+const bcrypt = require('bcryptjs');
 const pool = require('./db');
 const authMiddleware = require('./middleware/authMiddleware');
 
